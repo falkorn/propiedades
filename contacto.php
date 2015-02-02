b6afa2d70afbee476a8483b11bfcd099db023a36
falkorn <nicolas_ulloa_t@hotmail.com>
2015-02-02 11:36:17 -0300

    Contacto
    -Añadido pagina de contacto
    - Orden de carpetas htdocs

--- contacto.php
+++ contacto.php
@@ -1,126 +1,87 @@
 <!DOCTYPE html>
+
+<?php $pagina= 'contacto'; ?> <!-- Navegacion dinamica en el menu -->
+
 <html lang="es">
 <head>
-
 	<meta charset="utf-8">
 	<title>Torres Propiedades</title>		
-	<meta name="description" content="Departamentos en arriendo Concepcion, Casas en Arriendo Concepcion, Departamentos en venta Concepcion, Casas en venta Concepcion, Propiedades en concepcion, Departamento lomas de san andres, Casas lomas de san andres, Departamento Lomas de san sebastian, Casa Lomas de san sebastian, Corredor de Propidades Concepcion, Corredores de Propiedades Concepcion, Torres Propiedades, Torrespropiedades" />
+	<meta name="description" content="Casas, Departamentos y Terrenos en Venta y arriendo - Asesorias - Corredora de Propiedades - San Felipe" />
 	<meta name="viewport" content="width=device-width, minimum-scale=1, maximum-scale=1"/>
-	<meta name="author" content="Falkorn Design">
+	<meta name="author" content="Torres Propiedades">
 
-<!--********************
-    Hojas de Estilo
-*********************-->
-
-
-	<link rel="stylesheet" href="estilos.css">
-	<link rel="stylesheet" href="flexslider.css" type="text/css">
-
+	<link rel="stylesheet" href="css/bootstrap.css">
+	<link rel="stylesheet" href="css/styles.css">
 </head>
 
-<!-- *******************
-		Cuerpo
-*********************-->
+
+
 <body>
-		<!-- *******************************
-				Cabezera ( Header )
-		*********************************-->
-	<header>
-		<h1>
-			Torres Propiedades
-		</h1>
-		<figure id="logo">
-			<a href="#">
-			<img src="imagenes/logolargo.jpg" />
-			</a>
-		</figure>
-		<!-- *******************************
-				Barra de Navegacion
-		*********************************-->
-		<nav>
-			<ul>
-				<li><a href="index.html">Inicio</a></li>
-				<li><a href="ventas.html">Ventas</a></li>
-				<li><a href="arriendos.html">Arriendos</a></li>
-				<li><a href="contacto.html">Contacto</a></li>
-			</ul>
-		</nav>	
-	</header>
-
-	<section id="contenido">
-		<form action="" class="formulario">
-			<input type="text" id="nombre" placeholder="Nombre" required>
-			<input type="text" id="phone" Placeholder="Telefono" required>
-			<input type="text" id="correo" Placeholder="Correo" required>
-			<textarea name="mensaje" id="mensaje" Placeholder='Deje su mensaje aqui.' required></textarea>
-			<input type="submit" id="submit" value="Enviar">
-		</form>
-	</section>
-
-	<section id="contenido_contacto">
-		<article class="contacto">
-			<a href="#" class="titulo_item">Concepcion</a>
-			<p class="descripcion">Direccion: Jose de Garro #571, Lomas de San Andres Concepcion. <br>
-								   Fonos:     (041)2749852 - (09)51084857. <br>
-								   Correo:    nicolasulloa@torrespropiedades.cl</p>
-		</article>
-		<article class="contacto">
-			<a href="#" class="titulo_item">San Felipe</a>
-			<p class="descripcion">Direccion: Freire #751 Of. N°3, San Felipe. <br>
-								   Fonos:     (09)51378040. <br>
-								   Correo:    lauratorres@torrespropiedades.cl</p>
-		</article>
-		<article class="contacto">
-			<a href="#" class="titulo_item">Santiago</a>
-			<p class="descripcion">Direccion: Parcela 14, km. 35 camino Isla de Maipo, Santiago. <br>
-								   Fonos:     (09)79297701. <br>
-								   Correo:    josetorres@torrespropiedades.cl</p>
-		</article>
-	</section>	
-	<footer>
-		<p class="fono"><strong>Concepcion:</strong> (041)2749852 - (09)51084857 </p>
-		<p class="fono"><strong>Santiago:</strong>   (09)79297701 </p>
-		<p class="fono"><strong>San Felipe:</strong> (09)51378040 
-		</p>
-		<p>
-			Torres Propiedades
-		</p>
-	</footer>
+<!-- ********************* Header ***********************-->
+<?php require ('php/header.php') ?> 
 
 
-<!-- *******************
-		Scripts
-*********************-->
-  <!-- jQuery -->
-  <script src="http://ajax.googleapis.com/ajax/libs/jquery/1/jquery.min.js"></script>
-  <script>window.jQuery || document.write('<script src="js/libs/jquery-1.7.min.js">\x3C/script>')</script>
 
-  <!-- FlexSlider -->
-  <script defer src="js/jquery.flexslider.js"></script>
 
-	<script type="text/javascript">
-	    $(window).load(function(){
-	      $('.flexslider').flexslider({
-			    controlNav: false,
-			    touch: true,
-			    slideshow: false,
-	        start: function(slider){
-	          $('body').removeClass('loading');
-	        }
-	      });
-	    });
-	</script>
 
-	<script>
-	  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-	  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-	  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-	  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+<!-- *******************************
+		Contenedor Principal
+*********************************-->
+<div class="row">
+	<div class="col-xs-12 col-sm-6 col-sm-offset-3">
+<?php
+if (isset($_GET["status"])) 
+{
+	$status = $_GET["status"];
+	if($status == "success"){
+	echo '<div class="alert alert-success alert-dismissible fade in" role="alert">
+	<button type="button" class="close" data-dismiss="alert"><span aria-hidden="true">×</span><span class="sr-only">Close</span></button>
+	<p class="text-center"><strong>¡Mensaje enviado con Exito!</strong></p></div>';
+	}
+	if($status == "error"){
+	echo '<div class="alert alert-danger alert-dismissible fade in" role="alert">
+	<button type="button" class="close" data-dismiss="alert"><span aria-hidden="true">×</span><span class="sr-only">Close</span></button>
+	<p class="text-center"><strong>¡Error al enviar el mensaje!</strong></p></div>';
+	}
+} 
+?>
+		<h2 class="text-center">Contacto</h2>
+			<p class="contacto">
+				<i class="glyphicon glyphicon-earphone earphone"></i>
+				<b>
+					<a href="tel:+569 513 780 40">513 780 40</a> 
+				</b>
+			<p>
+		<div class="col-xs-12">
+			<form class="form-horizontal" role="form" method="get" action="php/correo.php">
 
-	  ga('create', 'UA-49363656-1', 'torrespropiedades.cl');
-	  ga('send', 'pageview');
+				<div class="form-group">
+					<input type="text" class="form-control" id="txt_nombre" name="txt_nombre" placeholder="Nombre">
+				</div>
+				<div class="form-group">
+					<input type="tel" class="form-control" pattern='\d{6}||\d{7}||\d{8}' id="txt_telephone" name="txt_telephone" placeholder="Telefono">
+				</div>				
+				<div class="form-group">
+					<input type="email" class="form-control" id="txt_email" name="txt_email" placeholder="Email">
+				</div>
+				<div class="form-group">
+					<input type="text" class="form-control" id="txt_asunto" name="txt_asunto" placeholder="Asunto">
+				</div>
+				<div class="form-group">
+					<textarea type="text" class="form-control" id="textarea_mensaje" rows="5" name="textarea_mensaje" placeholder="Escriba su mensaje"></textarea>
+				</div>
+				<div class="form-group">
+						<button type="submit" class="btn btn-primary pull-right enviar" name="btn_enviar_correo">Enviar</button>
+				</div>
+			</form>
+		</div>
 
-	</script>
+	</div>
+
+
+
+<!-- ********************* Footer  *************************-->
+<?php include_once ("php/footer.php"); ?>
 
 
 </body>
